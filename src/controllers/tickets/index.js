7f8a984d5a0990720bfae41680fbf29a647b0e9e
eomgn <matheeusgn21@gmail.com>
2025-06-11 10:18:09 -0300

    extraindo parametros passandos na URL que para isso é necessário criar um novo arquivo para extrarir atraves de um slice, split e reduce para criacao de um novo objeto com os parametros e logo utilizado em routeHandler para passar para rotas as rotas caso recebam parametros

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -8,6 +8,9 @@
 let route = "tickets";
 
 export function index({ request, response, database }) {
+  const { status } = request.query; // desestruturação, capturando o nome do parametro passado
+  console.log(status);
+
   let select = database.select(route);
 
   return response.writeHead(200).end(JSON.stringify(select));
--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -1,15 +1,24 @@
 import { routes } from "../routes/index.js";
 import { Database } from "../database/database.js";
+import { extractQueryParams } from "../utils/extractQueryParams.js";
 
 const database = new Database();
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    console.log(route);
     return route.method === request.method && route.path.test(request.url);
   });
 
   if (route) {
+    const routeParams = request.url.match(route.path);
+    // console.log(routeParams); // verificando os parametros passandos para a rota
+
+    // capturando os grupos de paramtros passados na rota
+    const { query } = routeParams.groups;
+    // console.log(extractQueryParams(query));
+
+    request.query = query ? extractQueryParams(query) : {}; // criando um novo 'metodo' em request
+
     return route.controller({ request, response, database });
   }
 
